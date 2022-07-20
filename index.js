ad019772043d5c115012f29d2fa21c4a46cc1fb6
Fernando Matzdorf <fernando@rindecuentas.org>
2022-07-20 18:39:26 -0500

    Add method for publicados transform.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ process.stdin
             return finalizadoDesiertoTransform(obj);
         case 'terminado_adjudicado':
             return terminadoAdjudicadoTransform(obj);
-        case 'publicados': // TODO
+        case 'publicados':
             return publicadoTransform(obj);
         default:
             return obj;
@@ -191,7 +191,50 @@ function terminadoAdjudicadoTransform(obj) {
     return removeNullFields(newObj);
 }
 
-function publicadoTransform(obj) {}
+function publicadoTransform(obj) {
+    let newObj = {};
+
+    // dateToISOString(obj[key])
+    // safeToString(obj[key])
+    // stringToFloat(obj[key])
+    Object.keys(obj).map( key => {
+        switch(key) {
+            case "AÑO DE CIERRE RECEPCIÓN":             newObj['anio_cierre_recepcion'] = parseInt(obj[key]); break;
+            case "AÑO DE PUBLICACIÓN":                  newObj['anio_publicacion'] = parseInt(obj[key]); break;
+            case "CATEGORIAS":
+            case "CATEGORÍAS":                          newObj['categorias'] = safeToString(obj[key]); break;
+            case "DESCRIPCION":
+            case "DESCRIPCIÓN":                         newObj['descripcion'] = safeToString(obj[key]); break;
+            case "ENTIDAD_COMPRADORA":
+            case "ENTIDAD COMPRADORA":                  newObj['entidad_compradora'] = safeToString(obj[key]); break;
+            case "ESTATUS":                             newObj['estatus_concurso'] = safeToString(obj[key]); break;
+            case "ESTATUS DEL CONCURSO":                newObj['estatus_concurso'] = safeToString(obj[key]); break;
+            case "FECHA CIERRE RECEPCIÓN OFERTAS":      newObj['fecha_cierre_recepcion_ofertas'] = dateToISOString(obj[key]); break;
+            case "FECHA_ADJUDICACION":                  newObj['fecha_adjudicacion'] = dateToISOString(obj[key]); break;
+            case "FECHA_PUBLICACION":
+            case "FECHA DE PUBLICACIÓN":                newObj['fecha_publicacion'] = dateToISOString(obj[key]); break;
+            case "FECHA DE ULTIMA ADJUDICACIÓN":        newObj['fecha_ultima_adjudicacion'] = dateToISOString(obj[key]); break;
+            case "NO":
+            case "FILA":                                newObj['fila'] = parseInt(obj[key]); break;
+            case "MES DE CIERRE RECEPCIÓN":             newObj['mes_cierre_recepcion'] = safeToString(obj[key]); break;
+            case "MES DE PUBLICACIÓN":                  newObj['mes_publicacion'] = safeToString(obj[key]); break;
+            case "MODALIDAD":                           newObj['modalidad'] = safeToString(obj[key]); break;
+            case "NOG_CONCURSO":
+            case "NOG CONCURSO":                        newObj['nog_concurso'] = safeToString(obj[key]); break;
+            case "SUB_MODALIDAD":
+            case "SUB MODALIDAD":                       newObj['sub_modalidad'] = safeToString(obj[key]); break;
+            case "TIPO_ENTIDAD":
+            case "TIPO ENTIDAD":                        newObj['tipo_entidad'] = safeToString(obj[key]); break;
+            case "TIPO_ENTIDAD_PADRE":
+            case "TIPO ENTIDAD PADRE":                  newObj['tipo_entidad_padre'] = safeToString(obj[key]); break;
+            case "TIPO_CONCURSO":                       newObj['tipo_concurso'] = safeToString(obj[key]); break;
+            case "UNIDAD_COMPRADORA":
+            case "UNIDAD COMPRADORA":                   newObj['unidad_compradora'] = safeToString(obj[key]); break;
+        }
+    } )
+
+    return removeNullFields(newObj);
+}
 
 function removeNullFields(object) {
     Object.keys(object).map( key => {
